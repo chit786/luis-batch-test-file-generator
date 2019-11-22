970339bb52939a9b4bc1bc53cf6e71247631d2f9
Chitrang Natu <chit786@gmail.com>
2019-11-22 11:58:40 +0100

    fix windows issue with invalid folder name

--- luis-batch-file-generator.js
+++ luis-batch-file-generator.js
@@ -203,8 +203,8 @@ dt = `${
     (dt.getMonth()+1).toString().padStart(2, '0')}_${
     dt.getDate().toString().padStart(2, '0')}_${
     dt.getFullYear().toString().padStart(4, '0')}_${
-    dt.getHours().toString().padStart(2, '0')}:${
-    dt.getMinutes().toString().padStart(2, '0')}:${
+    dt.getHours().toString().padStart(2, '0')}_${
+    dt.getMinutes().toString().padStart(2, '0')}_${
     dt.getSeconds().toString().padStart(2, '0')}`;
 
 fs.readdirSync(dataSetDir).forEach(file => {
